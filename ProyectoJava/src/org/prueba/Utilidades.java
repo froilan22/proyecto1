49af439cc2843ed5e46e0f130e75c89ba77479ba
Sergio Apaza <froilan22@gmail.com>
2015-10-13 15:58:35 -0500

    Prueba

--- ProyectoJava/src/org/prueba/Utilidades.java
+++ ProyectoJava/src/org/prueba/Utilidades.java
@@ -3,7 +3,7 @@ package org.prueba;
 public class Utilidades {
 
 	public static String enviarSaludo(String nombre) {
-		return "Bienvenido " + nombre;
+		return "Bienvenido xXXX " + nombre;
 	}
 
 }
