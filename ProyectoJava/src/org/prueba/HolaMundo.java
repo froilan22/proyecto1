c073e2e21b4b4206f525e11b1add4f2972693103
Sergio Apaza <froilan22@gmail.com>
2015-10-13 15:52:11 -0500

    Version Inicial del Proyecto

---
+++ ProyectoJava/src/org/prueba/HolaMundo.java
@@ -0,0 +1,13 @@
+package org.prueba;
+
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
---
+++ ProyectoJava/src/org/prueba/Utilidades.java
@@ -0,0 +1,9 @@
+package org.prueba;
+
+public class Utilidades {
+
+	public static String enviarSaludo(String nombre) {
+		return "Bienvenido " + nombre;
+	}
+
+}
